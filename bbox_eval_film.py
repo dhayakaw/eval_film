20e16120de69477c0c274945eb66ff973a542bf3
Daiki Hayakawa <daiki.hayakawa@cern.ch>
2022-07-25 22:05:27 +0900

    Implement basic functions

--- bbox_eval_film.py
+++ bbox_eval_film.py
@@ -1,6 +1,8 @@
 import cv2
 import numpy as np
 
+from sklearn.metrics import mean_squared_error
+
 img = cv2.imread('Image/img070.jpg')
 cv2.imshow('Original image', img)
 
@@ -9,11 +11,18 @@ ROI = cv2.selectROI('select ROI', img, fromCenter = False, showCrosshair = False
 print(ROI)
 
 img_crop = img[int(ROI[1]):int(ROI[1]+ROI[3]),int(ROI[0]):int(ROI[0]+ROI[2])]
-# img_crop = img[int(ROI[1]):int(ROI[3]),int(ROI[0]):int(ROI[2])]
+# cv2.imshow("crop", img_crop)
 
-print(img_crop)
+v = np.empty([ROI[3],ROI[2]])
+for i_w in range(ROI[2]):
+  for i_h in range(ROI[3]):
+    x = img_crop[i_h, i_w]
+    v[i_h, i_w] = sum(x)/3.0
 
-cv2.imshow("crop", img_crop)
-
+# print (v)
+print (np.mean(v))
+v_mean = np.full_like(v,np.mean(v))
+rmse = mean_squared_error(v, v_mean, squared = False)
+print (rmse)
 
 cv2.waitKey(0)
